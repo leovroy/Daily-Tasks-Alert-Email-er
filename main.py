18f6481d9ae04f3bea3cea3a88e068b2dbd9bfdb
leovroy <leovroy.46@gmail.com>
2022-01-14 20:43:39 +0530

    modified the mail.txt contents

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ data = pd.read_csv("info.csv")
 # create a dictionary containing tuples as keys and the entire row of dataframe as value using dict comprehension
 # key tuples should contain both the month and day in info.csv of respective tasks
 info_tuples = {(data_row['month'], data_row['day']):data_row for (index, data_row) in data.iterrows()}
+
+# check if the present tuple is present on the info_tuples dict
+if present_tuple in info_tuples:
+    # create a var called task_info which contains entire info of the present day tasks like the day, month ,tasks, year
+    task_info = info_tuples[(present_tuple)]
+    # if present_tuple in info_tuples, open the info mail.txt and read the lines replace [TASKS] with present days task
+    with open("mail.txt") as mail_file:
+        mail_contents = mail_file.read()
+        mail_contents = mail_contents.replace("[TASKS]", task_info["tasks"])
